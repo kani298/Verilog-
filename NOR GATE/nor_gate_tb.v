a7a32efa94de65fced657d64dbf604d917705ae2
kani298 <kanigarn@gmail.com>
2024-12-29 20:04:43 +0530

    Create nor_gate_tb.v

---
+++ NOR GATE/nor_gate_tb.v
@@ -0,0 +1,24 @@
+module mynor_tb;
+  reg a1,b1;
+  wire y1;
+  mynor mynor_tb(.a(a1),.b(b1),.y(y1));
+  initial begin
+    $monitor(a1,b1,y1);
+    a1=1'b0;
+    b1=1'b0;
+    #1 $display("a1:%b,b1:%b,y1:%b",a1,b1,y1);
+    #1
+    a1=1'b0;
+    b1=1'b1;
+    #1 $display("a1:%b,b1:%b,y1:%b",a1,b1,y1);
+    #1
+    a1=1'b1;
+    b1=1'b0;
+    #1 $display("a1:%b,b1:%b,y1:%b",a1,b1,y1);
+    #1
+    a1=1'b1;
+    b1=1'b1;
+    #1 $display("a1:%b,b1:%b,y1:%b",a1,b1,y1);
+ end  
+endmodule
+    
